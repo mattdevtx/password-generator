13cab80909327429a91156b07aa70a7aa6e38136
mattdevtx <matt@mattrodriguez.tech>
2024-04-16 09:00:40 -0500

    adds functionality to generate btn and password-display div

--- app.js
+++ app.js
@@ -1,3 +1,98 @@
 'use strict'
 
-const characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const characters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'!',
+	'@',
+	'#',
+	'$',
+	'%',
+	'&',
+	'*',
+	'(',
+	')',
+	'?',
+	'^',
+	':',
+	'[',
+	']',
+	'{',
+	'}',
+	0,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+]
+
+const displayPass = document.querySelector('.password')
+const generateBtn = document.querySelector('.generate-btn')
+
+generateBtn.addEventListener('click', () => {
+	const passWord = function (array) {
+		let newPass = ''
+		for (let i = 0; i <= 16; i++) {
+			let randoChar = Math.floor(Math.random() * characters.length - 1) + 1
+			newPass += `${array[randoChar]}`
+		}
+		return newPass
+	}
+	let pass = passWord(characters)
+	displayPass.textContent = pass
+})
